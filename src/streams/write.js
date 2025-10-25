c05fb894973bca677103c87084c0a2dc46ad6736
Алексей Шуклин <aleksejsuklin@MacBook-Pro-Aleksej.local>
2025-10-25 19:33:40 +0500

    feat: add read, write and transform functions

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import { createWriteStream } from "node:fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-  // Write your code here
+  const writeStream = createWriteStream(
+    path.resolve(__dirname, "files/fileToWrite.txt")
+  );
+
+  process.stdin.pipe(writeStream);
+
+  writeStream.on("open", () =>
+    console.log("Запись началась введите что-нибудь в консоль")
+  );
+
+  writeStream.on("finish", () =>
+    console.log("Запись завершена , хорошего дня тебе друг!")
+  );
 };
 
 await write();
