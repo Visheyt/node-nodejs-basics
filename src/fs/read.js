03eca2bfcda2cb41a1e6f71cd9b5a174a89b5b46
Алексей Шуклин <aleksejsuklin@MacBook-Pro-Aleksej.local>
2025-10-25 16:01:41 +0500

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,20 @@
-const read = async () => {};
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const read = async () => {
+  try {
+    const fileContent = await fs.readFile(
+      path.resolve(__dirname, "files/fileToRead.txt"),
+      { encoding: "utf-8" }
+    );
+    console.log(fileContent);
+  } catch {
+    throw new Error("FS operation failed");
+  }
+};
 
 await read();
