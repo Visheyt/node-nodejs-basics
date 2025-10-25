c70dceb49c50eead217fde5bcb74038032252c13
Алексей Шуклин <aleksejsuklin@MacBook-Pro-Aleksej.local>
2025-10-25 15:57:49 +0500

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-  // Write your code here
+  try {
+    const files = await fs.readdir(path.resolve(__dirname, "files"));
+    console.log(files);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
