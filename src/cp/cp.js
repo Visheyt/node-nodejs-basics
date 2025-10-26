9d9291f49f70ffbc2ccbb109d2ed4298b604a2a0
Алексей Шуклин <aleksejsuklin@MacBook-Pro-Aleksej.local>
2025-10-26 13:46:02 +0500

    feat: finish child process function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { spawn } from "node:child_process";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const child = spawn(
+    "node",
+    [path.join(__dirname, "files/script.js"), ...args],
+    {
+      stdio: ["pipe", "pipe", "inherit"],
+    }
+  );
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess(["arg1", "arg2"]);
